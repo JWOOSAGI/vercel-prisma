491b46ba3a294ec4f2a26bd96571ea6fef94a797
JWOOSAGI <ddgfg3234@naver.com>
2024-06-07 15:30:58 +0900

    change

---
+++ src/components/Homepage.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { Box } from "@mui/material"
+
+
+export default function Homepage() {
+  return(<>
+  <Box sx={{m:5}}>
+  회원가입
+</Box>
+<Box sx={{m:5}}>
+  로그인
+</Box></>)
+
+}
